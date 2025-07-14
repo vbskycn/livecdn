36f3fb06e474b16c9a8b40a931ea45ea096e1abe
vbskycn <34542650+vbskycn@users.noreply.github.com>
2025-07-14 10:17:50 +0800

    测试新方法

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+import { getAssetFromKV } from '@cloudflare/kv-asset-handler'
+
 export default {
   async fetch(request, env, ctx) {
-    return await env.ASSETS.fetch(request);
+    try {
+      return await getAssetFromKV({ request, waitUntil: ctx.waitUntil });
+    } catch (e) {
+      return new Response("资源未找到或出错", { status: 404 });
+    }
   }
-} 
+}
